1868fafdfe416a88086f5a793e7662ce8f0b7e4a
RC <airrudi@hotmail.com>
2016-11-28 12:23:03 +0100

    first eap try

--- src/main/java/nl/ruudclaassen/movie_list/App.java
+++ src/main/java/nl/ruudclaassen/movie_list/App.java
@@ -3,47 +3,73 @@ package nl.ruudclaassen.movie_list;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.format.Formatter;
 import org.springframework.format.support.FormattingConversionServiceFactoryBean;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import nl.ruudclaassen.movie_list.formatters.GenreFormatter;
+import nl.ruudclaassen.movie_list.web.formatters.GenreFormatter;
 
 // TODO: How to create a connection with the h2 database? 
 
 @EnableAutoConfiguration
 @ComponentScan
-public class App {
-	public static void main(String[] args){
+@SpringBootConfiguration
+public class App extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-	
+
 	@Autowired
+	@Lazy
 	private GenreFormatter genreFormatter;
 
-	@Bean
-	public LocalEntityManagerFactoryBean entityManagerFactoryBean(){
-		LocalEntityManagerFactoryBean localEntityManagerFactoryBean = new LocalEntityManagerFactoryBean();
-		localEntityManagerFactoryBean.setPersistenceUnitName("PersistenceUnit");
+	@Autowired
+	private DataSource dataSource;
 
-		return localEntityManagerFactoryBean;
-	}
-	
 	@Bean
-	public FormattingConversionServiceFactoryBean formattingService(){
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+		LocalContainerEntityManagerFactoryBean lcemfb = new LocalContainerEntityManagerFactoryBean();
+		lcemfb.setPackagesToScan("nl.ruudclaassen.movie_list.model");
+		lcemfb.setPersistenceUnitName("PersistenceUnit");
+		lcemfb.setJpaVendorAdapter(jpaVendorAdapter());
+		lcemfb.setDataSource(dataSource);
+		return lcemfb;
+	}
+
+	@Bean
+	public HibernateJpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter hjva = new HibernateJpaVendorAdapter();
+		hjva.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+		hjva.setGenerateDdl(true);
+		hjva.setShowSql(true);
+
+		return hjva;
+	}
+
+	@Bean
+	@Lazy
+	public FormattingConversionServiceFactoryBean formattingService() {
 		FormattingConversionServiceFactoryBean bean = new FormattingConversionServiceFactoryBean();
 		Set<Formatter> format = new HashSet<Formatter>();
 		format.add(genreFormatter);
 		bean.setFormatters(format);
-		
+
 		return bean;
-	}	
-	
-	
+	}
 
 }
